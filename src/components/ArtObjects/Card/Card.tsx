b7b07dfcb286fd221ca750554a3daf508efe8400
Ihèb BEN ROMDHANE <iheb02@yahoo.fr>
2024-07-08 18:14:47 +0200

    feat(ArtObjects): add Card component

---
+++ src/components/ArtObjects/Card/Card.tsx
@@ -0,0 +1,37 @@
+import { ArtObject } from '@/api/artObjects'
+import './styles.scss'
+import classNames from 'classnames'
+import { useState } from 'react'
+
+type Props = {
+  item: ArtObject
+  children: React.ReactNode
+}
+
+export function Card({ item }: Props) {
+  const [isLoaded, setIsLoaded] = useState(false)
+
+  const handleImageLoad = () => {
+    setIsLoaded(true)
+  }
+
+  return (
+    <li className='Card' tabIndex={0}>
+      <figure>
+        <img
+          alt={item.longTitle}
+          className={classNames('Card__image', {
+            'Card__image--isLoaded': isLoaded,
+          })}
+          loading='lazy'
+          src={item.webImage.url.replace('=s0', '')} // prevent loading original image
+          onLoad={handleImageLoad}
+        />
+
+        <figcaption className='Card__title'>
+          <span>{item.longTitle}</span>
+        </figcaption>
+      </figure>
+    </li>
+  )
+}
